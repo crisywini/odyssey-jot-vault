85f96d68b2213acb89798c5b60fd3f5d1c149525
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-11-27 11:46:23 -0500

    Added transportation jpa model

--- src/main/java/co/crisi/helidon/odyssey/jotvault/adapter/jpamodel/AccommodationJpaEntity.java
+++ src/main/java/co/crisi/helidon/odyssey/jotvault/adapter/jpamodel/AccommodationJpaEntity.java
@@ -24,7 +24,6 @@ public class AccommodationJpaEntity implements Serializable {
     @OneToMany
     private DestinationJpaEntity destination;
 
-
     private String name;
 
     private String address;
