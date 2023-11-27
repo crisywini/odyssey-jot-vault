fccceb865dda6f0b13b35d164ba18652146a47ba
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-11-27 11:40:49 -0500

    Added accommodation jpa model

---
+++ src/main/java/co/crisi/helidon/odyssey/jotvault/adapter/jpamodel/DestinationJpaEntity.java
@@ -0,0 +1,8 @@
+package co.crisi.helidon.odyssey.jotvault.adapter.jpamodel;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+public class DestinationJpaEntity implements Serializable {
+}
