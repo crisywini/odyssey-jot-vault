6f4c23e5c359d5c7a18a5d2a3aec28abdd469977
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-11-29 11:04:58 -0500

    ref

--- src/main/java/co/crisi/helidon/odyssey/jotvault/resources/filter/LoggingFilter.java
+++ src/main/java/co/crisi/helidon/odyssey/jotvault/resources/filter/LoggingFilter.java
@@ -3,7 +3,6 @@ package co.crisi.helidon.odyssey.jotvault.resources.filter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Priority;
-import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
