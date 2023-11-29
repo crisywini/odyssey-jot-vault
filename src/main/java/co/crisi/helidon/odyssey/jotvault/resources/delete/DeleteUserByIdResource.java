253c06811afc90ae65212323d95b5c9ddae41948
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-11-29 16:53:11 -0500

    Implemented basic simple circuit breaker omg

--- src/main/java/co/crisi/helidon/odyssey/jotvault/resources/delete/DeleteUserByIdResource.java
+++ src/main/java/co/crisi/helidon/odyssey/jotvault/resources/delete/DeleteUserByIdResource.java
@@ -1,6 +1,8 @@
 package co.crisi.helidon.odyssey.jotvault.resources.delete;
 
 import co.crisi.helidon.odyssey.jotvault.service.DeleteByIdProvider;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -21,10 +23,18 @@ public class DeleteUserByIdResource {
 
     @DELETE
     @Path("/{id}")
+    @CircuitBreaker(requestVolumeThreshold = 1, failureRatio = 0.5, delay = 1000, successThreshold = 2)
+    @Fallback(fallbackMethod = "tryAgain")
     public Response deleteById(@PathParam("id") Long id) {
         deleteUserByIdProvider.run(id);
         return Response.status(200)
                 .entity(String.format("User with id %d was removed", id))
+                .build();
+    }
+
+    public Response tryAgain(Long id) {
+        return Response.status(500)
+                .entity("Circuit is open, no further calls")
                 .build();
     }
 
